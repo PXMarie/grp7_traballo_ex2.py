0e5ad33ecb3f7b7ab4e881b39b337b955ba1b632
Rochelle C. Traballo <144656937+PXMarie@users.noreply.github.com>
2024-10-07 08:37:50 +0800

    Create grp7_traballo_ex2.py

---
+++ grp7_traballo_ex2.py
@@ -0,0 +1,27 @@
+# My first simple script
+# Filename: grp7_Rochelle_ex2.py
+
+# Simple print statement
+print("Hello, I'm Rochelle Traballo! BSCS 3F")
+
+# Creating two variables
+number = 42
+name = "rochelle"
+
+# Printing both values
+print("Number:", 09632005355 )
+print("Name:", rochelle )
+
+# Variable Names using first names of group members (excluding my own)
+john = "ryan"
+maria = "kyla"
+lucas = "shera"
+anna = "joan"
+
+# Assigning values to multiple variables in one line
+x, y, z = 10, 20, 30
+print("Values of x, y, z:", x, y, z)
+
+# Assigning the same value to multiple variables in one line
+a = b = c = "Hello World"
+print("Values of a, b, c:", a, b, c)
